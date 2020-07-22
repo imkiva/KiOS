228b3e9fa05299c32dbf102b5a600ec4665bfb65
imkiva <imkiva@icloud.com>
2020-07-22 15:56:43 +0800

    vga: screen and writer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+mod vga;
+
 extern crate rlibc;
 use core::panic::PanicInfo;
 
@@ -13,14 +15,5 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
     loop {}
 }
