4d878e64c262cc7c079c1785b28cfc3756e60974
imkiva <imkiva@icloud.com>
2020-07-22 16:04:39 +0800

    vga: print and println macros

--- src/vga.rs
+++ src/vga.rs
@@ -1,10 +1,26 @@
+use lazy_static::lazy_static;
 use spin::Mutex;
 use volatile::Volatile;
 
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: core::fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
+
 const BUFFER_HEIGHT: usize = 25;
 const BUFFER_WIDTH: usize = 80;
-
-use lazy_static::lazy_static;
 
 lazy_static! {
     pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
